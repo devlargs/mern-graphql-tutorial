2a5049e8b6514bdca3da112d54efb0155a7ac718
Ralph Largo <devlargs@gmail.com>
2019-10-03 01:51:09 +0800

    added initial graphql schema

---
+++ server/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema");
+
+const app = express();
+
+app.use("/graphql", graphqlHTTP({
+    schema
+}))
+
+const PORT = process.env.PORT || 8081;
+
+app.listen(PORT, console.log(`Listening to PORT ${PORT}`))
